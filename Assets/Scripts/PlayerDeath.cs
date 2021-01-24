419c8ec1883c181911029f9c3e2e2e4a3139401d
chsd60 <laptop@home>
2021-01-24 17:26:30 +0100

    Creating repository

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour {
+
+    public bool isDead = false;
+    public int Respawn;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            isDead = true;
+            StartCoroutine(DeadPlayer());
+        }
+    }
+
+    IEnumerator DeadPlayer() {
+        if (isDead == true) {
+            GameObject.Find("Player").SetActive(false);
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+}
