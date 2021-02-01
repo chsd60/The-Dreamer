4ecd801a6b838d9943d2df4e04176b63210d716c
chsd60 <laptop@home>
2021-02-01 12:28:05 +0100

    Misc

--- Assets/Scripts/ForcedDialogue.cs
+++ Assets/Scripts/ForcedDialogue.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForcedDialogue : MonoBehaviour
-{
+public class ForcedDialogue : MonoBehaviour {
+    private bool cutsceneHasHappened = false;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (cutsceneHasHappened == false) {
+            GetComponent<DialogSystem>().StartCoroutine("Type");
+            cutsceneHasHappened = true;
+        } else return;
+    }
     
 }
